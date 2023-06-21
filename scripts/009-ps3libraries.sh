bd3dc14ccace744f937a0deafce3b26ec15b0e0d
Phoenixx1202 <mcnet.fenix@gmail.com>
2023-06-21 04:37:31 +0000

    Update 009-ps3libraries.sh

--- scripts/009-ps3libraries.sh
+++ scripts/009-ps3libraries.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 # ps3libraries.sh by Naomi Peori (naomi@peori.ca)
 
-## Download the source code.
-wget --no-check-certificate https://github.com/bucanero/ps3libraries/tarball/master -O ps3libraries.tar.gz
+## Baixar o Código Fonte Atualizado.
+wget --no-check-certificate https://github.com/Phoenixx1202/ps3libraries/tarball/master -O ps3libraries.tar.gz
 
 ## Unpack the source code.
 rm -Rf ps3libraries && mkdir ps3libraries && tar --strip-components=1 --directory=ps3libraries -xvzf ps3libraries.tar.gz && cd ps3libraries
