1e3aecffb467685a00b18e3379910d5277720dfc
Phoenixx1202 <mcnet.fenix@gmail.com>
2025-09-27 12:07:31 -0400

    Update check-python.sh

--- depends/check-python.sh
+++ depends/check-python.sh
@@ -2,11 +2,11 @@
 # check-python.sh by Naomi Peori (naomi@peori.ca)
 
 ## Check for python.
-( python3 --version || python3 -V ) 1>/dev/null 2>&1 || { echo "ERROR: Install python before continuing."; exit 1; }
+( python --version || python -V ) 1>/dev/null 2>&1 || { echo "ERROR: Install python before continuing."; exit 1; }
 
 ## Check for python-config
-pyprefix=$(python3-config --prefix)
+pyprefix=$(python-config --prefix)
 [ $? -eq 0 ] || { echo "ERROR: Install python-dev before continuing."; exit 1; }
 
 ## Check for python header files
-( ls -1d "${pyprefix}"/include/python3.*/Python.h || ls -1d /opt/local/include/python3.*/Python.h ) 1>/dev/null 2>&1 || [ -f "$PYINSTALLDIR/include/Python.h" ] || { echo "ERROR: Install python3-dev before continuing."; exit 1; }
+( ls -1d "${pyprefix}"/include/python[23].*/Python.h || ls -1d /opt/local/include/python[23].*/Python.h ) 1>/dev/null 2>&1 || [ -f "$PYINSTALLDIR/include/Python.h" ] || { echo "ERROR: Install python-dev before continuing."; exit 1; }
