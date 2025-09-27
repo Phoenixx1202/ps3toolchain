14124d5ab8ca90d7e0f0a815e0a463dbfc6ec7c1
Phoenixx1202 <mcnet.fenix@gmail.com>
2025-09-27 12:43:17 -0400

    Update 007-gdb-SPU.sh

--- scripts/007-gdb-SPU.sh
+++ scripts/007-gdb-SPU.sh
@@ -1,19 +1,22 @@
 #!/bin/sh -e
 # gdb-SPU.sh by Naomi Peori (naomi@peori.ca)
 
-GDB="gdb-7.5.1"
+GDB="gdb-8.3.1"
 
 if [ ! -d ${GDB} ]; then
 
   ## Download the source code.
-  if [ ! -f ${GDB}.tar.bz2 ]; then wget --continue https://ftp.unicamp.br/pub/gnu/gdb/${GDB}.tar.bz2; fi
+  if [ ! -f ${GDB}.tar.gz ]; then wget --continue https://ftp.unicamp.br/pub/gnu/gdb/${GDB}.tar.gz; fi
 
   ## Download an up-to-date config.guess and config.sub
-  if [ ! -f config.guess ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.guess; fi
-  if [ ! -f config.sub ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.sub; fi
+  if [ ! -f config.guess ]; then wget --continue https://cgit.git.savannah.gnu.org/cgit/config.git/plain/config.guess; fi
+  if [ ! -f config.sub ]; then wget --continue https://cgit.git.savannah.gnu.org/cgit/config.git/plain/config.sub; fi
 
   ## Unpack the source code.
-  tar xfvj ${GDB}.tar.bz2
+  tar xfz ${GDB}.tar.gz
+
+  ## Patch the source code.
+  cat ../patches/${GDB}-PS3.patch | patch -p1 -d ${GDB}
 
   ## Replace config.guess and config.sub
   cp config.guess config.sub ${GDB}
@@ -34,9 +37,10 @@ cd ${GDB}/build-spu
 ../configure --prefix="$PS3DEV/spu" --target="spu" \
     --disable-nls \
     --disable-sim \
-    --disable-werror
+    --disable-werror \
+    --without-python
 
 ## Compile and install.
-PROCS="$(nproc --all 2>&1)" || ret=$?
-if [ ! -z $ret ]; then PROCS=4; fi
+PROCS="$(grep -c '^processor' /proc/cpuinfo 2>/dev/null)" || ret=$?
+if [ ! -z $ret ]; then PROCS="$(sysctl -n hw.ncpu 2>/dev/null)"; fi
 ${MAKE:-make} -j $PROCS && ${MAKE:-make} libdir=host-libs/lib install
